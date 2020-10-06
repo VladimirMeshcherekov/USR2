680fdfc08d8ba727308730f8b56ece5b9b17ecc8
user <3gotosnapp@gmail.com>
2020-10-06 20:49:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Первое число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Второе число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if ((a == b) & (a != b))
+            { // строки сравниваюся по их длинне (в теории)
+                Console.WriteLine("Невозможно ");
+            }
+
+            if ((a == b) | (a != b))
+            { // строки сравниваюся по их длинне
+                Console.WriteLine("ну, либо они равны, либо нет ");
+            }
+
+            if (a > b)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
